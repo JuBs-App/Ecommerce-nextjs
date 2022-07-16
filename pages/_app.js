3f2782a7053cbbf8e0a87705f4a0dc649c3c7f81
jubin369 <jubinayoob369@gmail.com>
2022-07-16 20:39:30 +0530

    Adds store

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/globals.css'
 import theme from '../styles/theme'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { StoreProvider } from '../utils/Store'
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <StoreProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </StoreProvider>
     </ChakraProvider>
   )
 }

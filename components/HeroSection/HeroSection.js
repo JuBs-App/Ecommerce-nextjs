f41600b8c253fb7118c1b6107840a3b22d8978df
jubin369 <jubinayoob369@gmail.com>
2022-07-29 20:03:56 +0530

    Style fix

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Button, Carousel } from 'react-bootstrap'
-import { Heading, Image, Stack, Text } from '@chakra-ui/react'
+import { Heading, Image, ScaleFade, Stack, Text } from '@chakra-ui/react'
 import AOS from 'aos'
 // import 'aos/dist/aos.css'
 // import "./HeroSection.css";
@@ -22,25 +22,28 @@ const HeroSection = () => {
             className=""
             src="/Goa-natural/banner/safflower.jpg"
             alt="Safflower"
+            height={['200px', '260px', '400px', '450px']}
           />
           <Carousel.Caption>
-            <p
-              className="carousel-head saff-head"
-              data-aos="fade-up"
-              data-aos-duration="2000"
-            >
-              Safflower Oil
-            </p>
-            <p
-              className="carousel-desc saff-desc"
-              data-aos="fade-up"
-              data-aos-duration="2000"
-            >
-              Keeps the Heart Healthy
-            </p>
-            <Button data-aos="fade-up" data-aos-duration="3000">
-              Buy Now
-            </Button>
+            <ScaleFade initialScale={0.9} in="true">
+              <Text
+                className="carousel-head saff-head"
+                data-aos="fade-up"
+                data-aos-duration="2000"
+              >
+                Safflower Oil
+              </Text>
+              <Text
+                className="carousel-desc saff-desc"
+                data-aos="fade-up"
+                data-aos-duration="2000"
+              >
+                Keeps the Heart Healthy
+              </Text>
+              <Button data-aos="fade-up" data-aos-duration="3000">
+                Buy Now
+              </Button>
+            </ScaleFade>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -48,30 +51,33 @@ const HeroSection = () => {
             className=""
             src="/Goa-natural/banner/turmeric.jpg"
             alt="Turmeric"
+            height={['200px', '260px', '400px', '450px']}
           />
 
           <Carousel.Caption>
-            <p
-              className="carousel-head tur-head"
-              data-aos="fade-up"
-              data-aos-duration="2000"
-            >
-              Turmeric Powder
-            </p>
-            <p
-              className="carousel-desc tur-desc"
-              data-aos="fade-up"
-              data-aos-duration="2000"
-            >
-              Include Spoonful illusion in your food
-            </p>
-            <Button
-              data-aos="fade-up"
-              data-aos-duration="3000"
-              className="tur-btn"
-            >
-              Buy Now
-            </Button>
+            <ScaleFade initialScale={0.9} in="true">
+              <Text
+                className="carousel-head tur-head"
+                data-aos="fade-up"
+                data-aos-duration="2000"
+              >
+                Turmeric Powder
+              </Text>
+              <Text
+                className="carousel-desc tur-desc"
+                data-aos="fade-up"
+                data-aos-duration="2000"
+              >
+                Include Spoonful illusion in your food
+              </Text>
+              <Button
+                data-aos="fade-up"
+                data-aos-duration="3000"
+                className="tur-btn"
+              >
+                Buy Now
+              </Button>
+            </ScaleFade>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -79,26 +85,29 @@ const HeroSection = () => {
             className=""
             src="/Goa-natural/banner/groundnut.jpg"
             alt="Groundnut"
+            height={['200px', '260px', '400px', '450px']}
           />
 
           <Carousel.Caption>
-            <p
-              className="carousel-head gnd-head"
-              data-aos="fade-up"
-              data-aos-duration="2000"
-            >
-              Groundnut Oil
-            </p>
-            <p
-              className="carousel-desc gnd-desc"
-              data-aos="fade-up"
-              data-aos-duration="2000"
-            >
-              Dont't Compromise with Health And Taste
-            </p>
-            <Button data-aos="fade-up" data-aos-duration="3000">
-              Buy Now
-            </Button>
+            <ScaleFade initialScale={0.9} in="true">
+              <Text
+                className="carousel-head gnd-head"
+                data-aos="fade-up"
+                data-aos-duration="2000"
+              >
+                Groundnut Oil
+              </Text>
+              <Text
+                className="carousel-desc gnd-desc"
+                data-aos="fade-up"
+                data-aos-duration="2000"
+              >
+                Dont't Compromise with Health And Taste
+              </Text>
+              <Button data-aos="fade-up" data-aos-duration="3000">
+                Buy Now
+              </Button>
+            </ScaleFade>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -106,25 +115,28 @@ const HeroSection = () => {
             className=""
             src="/Goa-natural/banner/coconut.jpg"
             alt="Coconut"
+            height={['200px', '260px', '400px', '450px']}
           />
           <Carousel.Caption>
-            <p
-              className="carousel-head coco-head"
-              data-aos="fade-up"
-              data-aos-duration="2000"
-            >
-              Coconut Oil
-            </p>
-            <p
-              className="carousel-desc coco-desc"
-              data-aos="fade-up"
-              data-aos-duration="2000"
-            >
-              The Real Super Food From Nature
-            </p>
-            <Button data-aos="fade-up" data-aos-duration="3000">
-              Buy Now
-            </Button>
+            <ScaleFade initialScale={0.9} in="true">
+              <Text
+                className="carousel-head coco-head"
+                data-aos="fade-up"
+                data-aos-duration="2000"
+              >
+                Coconut Oil
+              </Text>
+              <Text
+                className="carousel-desc coco-desc"
+                data-aos="fade-up"
+                data-aos-duration="2000"
+              >
+                The Real Super Food From Nature
+              </Text>
+              <Button data-aos="fade-up" data-aos-duration="3000">
+                Buy Now
+              </Button>
+            </ScaleFade>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
